434467f8a8f4ebfaaca2bcd7bc1c05b32a295f7f
Sukhvansh2004 <120120146+Sukhvansh2004@users.noreply.github.com>
2024-01-27 17:46:50 +0530

    STVL

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,15 @@
+# CMake generated Testfile for 
+# Source directory: /home/sukhvansh/Husky/src
+# Build directory: /home/sukhvansh/Husky/src/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("husky_desktop")
+subdirs("husky_simulator")
+subdirs("husky_msgs")
+subdirs("husky_control")
+subdirs("husky_description")
+subdirs("husky_gazebo")
+subdirs("husky_navigation")
+subdirs("husky_viz")
