0fc88a9093767c4420c9d88ab91c39a0c34c1503
Sukhvansh2004 <120120146+Sukhvansh2004@users.noreply.github.com>
2024-02-04 15:55:55 +0530

    Spiral Search

--- marker-detection/scripts/spiral_search.py
+++ marker-detection/scripts/spiral_search.py
@@ -1,67 +1,82 @@
 #!/usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import PoseStamped, Twist
+from geometry_msgs.msg import PoseStamped, Point
 from nav_msgs.msg import Odometry
+from tf.transformations import quaternion_from_euler
 import math
 
 class SpiralWaypointGenerator:
     def __init__(self):
-        rospy.init_node('spiral_waypoint_generator')
+        rospy.init_node('spiral_waypoint_generator', anonymous=True)
 
-        # Parameters
+        self.threshold_distance = 2
         self.initial_pose = None
-        self.spiral_radius = 1.0  # Adjust this based on your requirement
-        self.spiral_distance_increment = 0.1  # Adjust this based on your requirement
-        self.threshold_distance = 0.2  # Adjust this based on your requirement
-        self.next_pose = None
-        # Publishers and Subscribers
-        self.waypoint_pub = rospy.Publisher('/output_point', PoseStamped, queue_size=1)
-        self.odom_sub = rospy.Subscriber('/odometry/filtered', Odometry, self.odometry_callback)
-
-    def odometry_callback(self, odom):
-        if self.initial_pose is None:
-            self.initial_pose = odom.pose.pose
-
-        if self.next_pose:
-            current_distance = self.calculate_distance(self.next_pose, odom.pose.pose)
-
-            if current_distance < self.threshold_distance:
-                next_waypoint = self.calculate_spiral_waypoint(odom.pose.pose)
-                # self.publish_waypoint(next_waypoint)
-                print("Next forward \n", self.next_pose)
-        else:
-            next_waypoint = self.calculate_spiral_waypoint(odom.pose.pose)
-            # self.publish_waypoint(next_waypoint)
-            print("Next first", self.next_pose)
+        self.spiral_radius = 20
+        self.step_increment = 1
+        self.waypoints = self.generate_spiral_waypoints()
+        self.current_position = None
         
-    def calculate_distance(self, pose1, pose2):
-        x1, y1 = pose1.position.x, pose1.position.y
-        x2, y2 = pose2.position.x, pose2.position.y
-        return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+        self.odom_subscriber = rospy.Subscriber('/odometry/filtered', Odometry, self.odom_callback)
+        self.goal_publisher = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
 
-    def calculate_spiral_waypoint(self, current_pose):
-        current_x, current_y = current_pose.position.x, current_pose.position.y
+    def generate_spiral_waypoints(self):
+        waypoints = []
+        t = 1
+        while t <= self.spiral_radius:
+            x = math.cos(t) * t / 2
+            y = math.sin(t) * t / 2
 
-        angle = math.atan2(current_y - self.initial_pose.position.y, current_x - self.initial_pose.position.x)
-        next_x = current_x + self.spiral_distance_increment * math.cos(angle)
-        next_y = current_y + self.spiral_distance_increment * math.sin(angle)
+            waypoint = PoseStamped()
+            waypoint.header.frame_id = 'odom'
+            waypoint.pose.position = Point(x, y, 0.0)
+            
+            tangent_angle = math.atan2(y + t * math.cos(t), x - t * math.sin(t))
+            quaternion = quaternion_from_euler(0, 0, tangent_angle)
+            waypoint.pose.orientation.x = quaternion[0]
+            waypoint.pose.orientation.y = quaternion[1]
+            waypoint.pose.orientation.z = quaternion[2]
+            waypoint.pose.orientation.w = quaternion[3]
 
-        waypoint = PoseStamped()
-        waypoint.header.frame_id = 'odom'
-        waypoint.pose.position.x = next_x
-        waypoint.pose.position.y = next_y
-        waypoint.pose.orientation.w = 1.0  # Assuming no rotation needed
-        self.next_pose = waypoint.pose
+            waypoints.append(waypoint)
+
+            t += self.step_increment 
         
-        return waypoint
+        return waypoints
 
-    def publish_waypoint(self, waypoint):
-        self.waypoint_pub.publish(waypoint)
+    def odom_callback(self, odom_msg):
+        if(odom_msg.header.stamp.secs != rospy.Time.now().secs):
+            return None
+        self.current_position = odom_msg.pose.pose.position
+        rate = rospy.Rate(2)
+        if not self.initial_pose:
+            self.initial_pose = self.current_position
+            temp = PoseStamped()
+            temp.pose.position.x += self.initial_pose.x
+            temp.pose.position.y += self.initial_pose.y
+            temp.header.frame_id = 'odom'
+            temp.header.stamp = rospy.Time.now()
+            self.waypoints.insert(0, temp)
+        
+        rate.sleep()
+        if len(self.waypoints) > 0:
+            next_waypoint = self.waypoints[0].pose.position
+
+            distance = math.sqrt((self.current_position.x - next_waypoint.x)**2 +
+                                 (self.current_position.y - next_waypoint.y)**2)
+            
+            if distance < self.threshold_distance:
+                self.waypoints.pop(0)
+                if len(self.waypoints) > 0:
+                    goal_msg = self.waypoints[0]
+                    goal_msg.pose.position.x += self.initial_pose.x
+                    goal_msg.pose.position.y += self.initial_pose.y
+                    goal_msg.header.stamp = rospy.Time.now()
+                    self.goal_publisher.publish(goal_msg)
+
+    def run(self):
+        rospy.spin()
 
 if __name__ == '__main__':
-    try:
-        spiral_generator = SpiralWaypointGenerator()
-        rospy.spin()
-    except rospy.ROSInterruptException:
-        pass
+    spiral_generator = SpiralWaypointGenerator()
+    spiral_generator.run()
